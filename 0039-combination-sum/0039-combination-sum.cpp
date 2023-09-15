eb7f1a42dba6e5abf42378784c6338a5ce522106
GopiPallapu <140641025+GopiPallapu@users.noreply.github.com>
2023-09-15 11:30:08 +0530

    Time: 0 ms (100%), Space: 11.1 MB (46.5%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
-    void helper(int i , int n, int target ,vector<int> &candidates ,vector<vector<int>> &ans, vector<int> &subset , int sum)
+    void helper(int i , int n, int target ,vector<int> &candidates ,vector<vector<int>> &ans, vector<int> &subset )
     {
-        if(sum == target)
+        // base case
+        if(i == n)
         {
-            ans.push_back(subset);
+            if(target == 0)
+            {
+                ans.push_back(subset);
+            }
             return;
         }
-        if(sum > target)return ;
-        if( i >= n)return ;
         // take ith value
-        sum += candidates[i];
-        subset.push_back(candidates[i]);
-        //  we are not inc i value because i may be included more times
-        helper(i, n , target, candidates , ans, subset, sum);
-        // skip ith value
-        sum -=candidates[i];
-        subset.pop_back();
-         helper(i+1, n , target, candidates , ans, subset, sum); 
+        if(candidates[i] <= target)
+        {
+            subset.push_back(candidates[i]);
+             //  we are not inc i value because i may be included more times
+            helper(i, n , target-candidates[i], candidates , ans, subset);
+            // skip ith value
+            subset.pop_back();
+        }
+       
+         helper(i+1, n , target, candidates , ans, subset); 
 
          return ;
     }
@@ -25,7 +29,7 @@ public:
         vector<vector<int>> ans;
         vector<int> subset;
         int n = candidates.size();
-        helper(0,n,target,candidates , ans,subset , 0);
+        helper(0,n,target,candidates , ans,subset);
         return ans;
     }
 };
