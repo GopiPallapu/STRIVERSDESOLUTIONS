d86b716fc4ebfd5b8100e206f7822eb00632aff1
GopiPallapu <140641025+GopiPallapu@users.noreply.github.com>
2023-09-19 15:22:44 +0530

    Time: 2 ms (53.91%), Space: 6.2 MB (9.41%) - LeetHub

--- 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
@@ -3,42 +3,34 @@ public:
     // 324
     // 3,2,4
     int nextGreaterElement(int x) {
-        vector<int>nums;
-        while(x>0)
-        {
-            nums.push_back(x%10);
-            x/=10;
-        } 
-        int i;
-        int n = nums.size();
-        reverse(nums.begin() , nums.end());
-        for(i = n-1 ; i > 0 ;i--)
-        {
-            if(nums[i -1] < nums[i])
-            {
-                break;
-            }
-        }
-        if(i == 0)
-            return -1;
-        int element = nums[i-1];
-        int smallest =i;
-        for(int j = i+1 ; j< n;j++)
-        {
-            if(nums[j] > element && nums[j]<= nums[smallest] )
-            {
-                smallest = j;
-            }
-        }
-        swap(nums[i-1], nums[smallest]);
-        sort(nums.begin()+i,nums.end());
-        
-        long long int answer = 0;
-        for(int i =0; i<n ; i++)
-        {
-            answer = answer * 10 ;
-            answer +=nums[i];
-        }
-        return (answer > INT_MAX) ? -1 : (int)answer;
+       string str = to_string(x);
+       int i;
+       int n = str.size();
+       for( i = n-1 ;i>0 ;i--)
+       {
+           if(str[i-1] < str[i])
+           {
+               break;
+           }
+       }
+       if(i == 0)
+       {
+           return -1;
+       }
+       int element = str[i-1];
+       int smallest = i ;
+       for(int j = i+1 ; j< n;j++)
+       {
+           if(str[j] > element && str[j] <= str[smallest])
+           {
+               smallest = j;
+           }
+       }
+       swap(str[i-1] , str[smallest]);
+       sort(str.begin()+i , str.end());
+       long  ans = stol(str);
+      if(ans > INT_MAX)return -1;
+      else
+        return (int)ans;
     }
 };
