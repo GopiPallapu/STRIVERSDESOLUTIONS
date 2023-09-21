f3275d83615e23f84c7013966f838b17d127ea08
GopiPallapu <140641025+GopiPallapu@users.noreply.github.com>
2023-09-21 11:20:10 +0530

    Time: 18 ms (14.28%), Space: 9.2 MB (20.24%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -6,27 +6,32 @@ public:
         st.push(num[0]);
         for(int i =1 ;i < num.size() ; i++)
         {
-            while(!st.empty() && st.top() > (num[i]) && k !=0) // stack - 1, 2, 1, 9
+            while(!st.empty() && st.top() > (num[i]) && k !=0) // stack - 0 , 
             {
                 st.pop();
                 k--;
             }
-            st.push((num[i]));
+            st.push((num[i])); 
+            // this will be used to remove any leading zeros before
             if(st.size() == 1 && num[i] == '0')
-                st.pop();
+               st.pop();
         }
+         // incase the stack is increasing order then no digit were popped till now . 
+         // then we have to remove last k digits
+         // eg : 14679 , then stack contains all digits .
         while(k > 0 && !st.empty())
         {
             st.pop() ;
             k--;
         }
+        // push final stack nums to answer and reverse the string
         while(!st.empty()) 
         {
             answer += st.top() ;
             st.pop() ;
         }
         reverse(answer.begin() , answer.end());
-        if(answer.size() == 0)
+        if(answer.size() == 0) // base case
             answer = "0";
         return answer ;
     }
