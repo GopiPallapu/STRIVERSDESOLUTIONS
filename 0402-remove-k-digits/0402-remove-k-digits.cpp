b665f1780626037cf7446c0a14615e59070b52b3
GopiPallapu <140641025+GopiPallapu@users.noreply.github.com>
2023-09-21 23:00:37 +0530

    Time: 14 ms (42.27%), Space: 8.9 MB (44.28%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
+		 // number of operation greater than length we return an empty string
+        if(num.length() <= k)   
+            return "0";
         string answer = "" ;
         stack<char> st ;
-        st.push(num[0]);
-        for(int i =1 ;i < num.size() ; i++)
+        //st.push(num[0]);
+        for(int i =0 ;i < num.size() ; i++)
         {
-            while(!st.empty() && st.top() > (num[i]) && k !=0) // stack - 0 , 
+            while(!st.empty() && st.top() > (num[i]) && k !=0) // stack - 1, 2, 1, 9
             {
                 st.pop();
                 k--;
             }
-            st.push((num[i])); 
-            // this will be used to remove any leading zeros before
-            if(st.size() == 1 && num[i] == '0')
-               st.pop();
+            if(num[i]  != '0') // ignore zeros
+                    st.push((num[i]));
+            else if(num[i] == '0' && !st.empty()) // add zeros if stack is not empty
+                st.push(num[i]);
         }
          // incase the stack is increasing order then no digit were popped till now . 
          // then we have to remove last k digits
