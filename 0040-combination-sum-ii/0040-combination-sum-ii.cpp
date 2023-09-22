e55bbe7a49f0495ffdd2d1a521a2eee80bf9280e
GopiPallapu <140641025+GopiPallapu@users.noreply.github.com>
2023-09-22 15:45:58 +0530

    Time: 0 ms (100%), Space: 11.1 MB (27.62%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,32 +1,42 @@
 class Solution {
 public:
-    void helper(int i , int n, int target ,vector<int> &candidates ,vector<vector<int>> &ans, vector<int> &subset , int sum)
+/*Input: candidates = [10,1,2,7,6,1,5], target = 8
+Output: 
+[
+[1,1,6],
+[1,2,5],
+[1,7],
+[2,6]
+]*/
+void helper(int i , int n , int target , vector<int>& set , vector<vector<int>>& answer , vector<int>& candidates ,int  sum)
+{
+    // base case
+    if(sum == target)
     {
-        if(sum == target)
-        {
-            ans.push_back(subset);
-            return;
-        }
-        if(sum > target)return ;
-        if( i >= n)return ;
-        // take ith value
-        sum += candidates[i];
-        subset.push_back(candidates[i]);
-        helper(i+1, n , target, candidates , ans, subset, sum);//  we are not inc i value because i may be included more times
-        // skip ith value
-        sum -=candidates[i];
-        subset.pop_back();
-         while(i+1 < n && candidates[i] == candidates[i+1])i++;
-         helper(i+1, n , target, candidates , ans, subset, sum); 
+        answer.push_back(set);
+        return ;
+    }
+    if(sum > target )return ;
+    if(i >= n) return;
+    
+    sum +=candidates[i] ;
+    set.push_back(candidates[i]);
+    helper(i+1 , n , target , set , answer , candidates , sum);
+    
+    sum -= candidates[i] ;
+    set.pop_back() ;
+    while(i+1 < n && candidates[i] == candidates[i+1])i++ ;
+     helper(i+1 , n , target , set , answer , candidates , sum);
+     
+     return ;
+}
+vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+       int n = candidates.size() ;
+       vector<vector<int>> answer ;
+       vector<int> set ;
+       sort(candidates.begin() , candidates.end()) ;
+       helper( 0 , n , target ,set , answer  , candidates , 0);
+       return answer;
+}
 
-         return ;
-    }
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-         vector<vector<int>> ans;
-        vector<int> subset;
-        int n = candidates.size();
-        sort(candidates.begin() , candidates.end());
-        helper(0,n,target,candidates , ans,subset , 0);
-        return ans;
-    }
 };
