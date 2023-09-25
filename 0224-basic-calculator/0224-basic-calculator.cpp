6c0b771a5c1d07b9ca8c9a2f90bf90a2a8ab5999
GopiPallapu <140641025+GopiPallapu@users.noreply.github.com>
2023-09-25 15:41:47 +0530

    Time: 4 ms (88.61%), Space: 8.3 MB (37.05%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int calculate(string s) {
+        int n = s.size() ;
+        int result =0;
+        int sign =1;
+        stack<int> st ;
+        int num =0;
+        for(int i =0;i< n ; i++)
+        {
+            if(isdigit(s[i]))
+            {  
+                num = num * 10 + (s[i]-'0');
+            }
+            else if(s[i] == '+')
+            {
+                result  += num * sign; 
+                num =0;
+                sign =1;
+            }
+            else if(s[i] =='-')
+            {
+                result  += num * sign; 
+                num =0;
+                sign =-1;
+            }
+            else if(s[i] == '(')
+            {
+                st.push(result) ;
+                st.push(sign) ;
+                result =0;
+                num =0;
+                sign =1 ;
+            }
+            else if(s[i] ==')')
+            {
+                result  += num * sign; 
+                num =0;
+                sign = 1;
+                int oldSign = st.top() ;
+                st.pop() ;
+                int oldResult = st.top() ;
+                st.pop() ;
+                result *= oldSign  ;
+                result += oldResult ;
+            }
+        }
+        result += (num*sign );
+        return result;
+    }
+};
