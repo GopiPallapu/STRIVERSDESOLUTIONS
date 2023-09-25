fd5a06e9586b873a25eceec1fcf6e5f9910eb6a1
GopiPallapu <140641025+GopiPallapu@users.noreply.github.com>
2023-09-25 12:27:39 +0530

    Time: 36 ms (15.68%), Space: 89.7 MB (46.29%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -4,46 +4,51 @@ public:
     // TC -> O(log(min(n1,n2)))
     // SC -> O(1)
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-
-        if( n1 > n2)return findMedianSortedArrays(nums2, nums1);
-
-        int n = n1+n2;
-
-        int partition = (n+1)/2;
-         // edge cases
-        if(n1==0)
-            return n2%2?nums2[n2/2]:(nums2[n2/2]+nums2[n2/2-1])/2.0;  
-        if(n2==0)
-            return n1%2?nums1[n1/2]:(nums1[n1/2]+nums1[n1/2-1])/2.0;
-        int left =0 ;
-        int right =n1;
-        int cut1 =0 , cut2 =0;
+       int n1 = nums1.size() ;
+       int n2 = nums2.size() ;
+       if(n1 > n2)
+            return findMedianSortedArrays(nums2 , nums1) ;
+        int n = (n1+n2) ;
+        
+        int partiton = (n+1)/2 ;
+        
+        int cut1 =0;
+        int cut2 =0;
+        int left =0;
+        int right = n1 ;
         while(left <= right)
         {
-            cut1 =(left+right)/2 ;
-            cut2 = partition-cut1; 
-
-            int l1 = cut1 == 0 ? INT_MIN :nums1[cut1-1];
-            int l2 = cut2 == 0 ? INT_MIN : nums2[cut2-1];
-            int r1 = cut1 >= n1 ? INT_MAX : nums1[cut1];
-            int r2 = cut2 >= n2 ? INT_MAX : nums2[cut2];
-
+            int mid = (left+right)/2 ;
+            cut1 = mid ;
+            cut2 = partiton - cut1 ;
+            
+            int l1 = cut1 == 0 ? INT_MIN : nums1[cut1-1] ;
+            int r1 = cut1 >= n1 ? INT_MAX : nums1[cut1] ;
+            
+            int l2 = cut2 ==0 ? INT_MIN : nums2[cut2-1] ;
+            int r2 = cut2 >= n2 ? INT_MAX : nums2[cut2] ;
+            
+            // correct partition
             if(l1 <= r2 && l2 <= r1)
             {
-                if(n %2 == 1)
+                if( n%2 == 1)
+                {
                     return max(l1 , l2);
+                }
                 else
                 {
-                    return (max(l1,l2)+min(r1,r2))/2.0;
+                    return (max(l1 ,l2)+ min(r1,r2))/2.0 ;
                 }
             }
-            else if( l1 > r2 )
-                right = cut1-1;
+            else if( l1 > r2)
+            {
+                right = cut1-1 ;
+            }
             else
-                left = cut1 + 1 ;
+            {
+                left = cut1+1 ;
+            }
         }
-        return 0.0;
+        return 0.0 ;
     }
 };
