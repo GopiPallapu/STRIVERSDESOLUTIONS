6bbf33bc4582a3e2e515aa6da2d983b582ef2bb4
GopiPallapu <140641025+GopiPallapu@users.noreply.github.com>
2023-09-25 14:05:03 +0530

    Time: 7 ms (64.75%), Space: 11.9 MB (5.32%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -20,63 +20,23 @@ public:
 
 class Solution {
 public:
-    void copy(Node* head)
-    {
-        Node* temp  =head , *n = head->next;
-        while(temp != NULL)
-        {
-            Node* node = new Node(temp->val);
-            node->next = n;
-            temp->next = node;
-            temp  = n;
-            if(n)n = n->next;
-        }
-    }
-    void random(Node* head)
-    {
-        Node* temp = head;
-        while(temp != NULL)
-        {
-                if(temp->random != NULL)
-                {
-                    temp->next->random = temp->random->next;
-                }
-                temp  =temp->next->next;
-        }
-    }
-    Node* detach(Node* head)
-    {
-        Node* dummy = new Node(-1);
-        Node* tail = dummy  ,*temp = head;
-        while(temp != NULL)
-        {
-                tail->next = temp->next;
-                tail = tail->next;
-                temp->next = tail->next;
-                temp = temp->next;
-        }
-        return dummy->next;
-    }
     Node* copyRandomList(Node* head) {
-        if(head == NULL)return head;
-       /* unordered_map<Node*, Node*> mp;
-        Node* temp = head;
+        if(head == NULL)return head ;
+        unordered_map <Node* , Node*> mp ;
+        Node* temp = head ;
         while(temp != NULL)
         {
-            Node* newNode =  new Node(temp->val);
-            mp[temp] = newNode;
-            temp = temp->next;
+            Node* newNode =  new Node(temp->val) ;
+            mp[temp] = newNode ;
+            temp = temp->next ;
         }
-        temp = head;
+        temp= head;
         while(temp != NULL)
         {
-            mp[temp]->next = mp[temp->next];
-            mp[temp]->random = mp[temp->random];
-            temp = temp->next;
+            mp[temp]->next = mp[temp->next] ;
+            mp[temp]->random = mp[temp->random] ;
+            temp = temp->next ;
         }
-        return mp[head];*/
-        copy(head);
-        random(head);
-        return detach(head);
+        return mp[head] ; 
     }
 };
