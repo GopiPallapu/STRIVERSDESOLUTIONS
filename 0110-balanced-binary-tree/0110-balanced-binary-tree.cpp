60f41665abc06a8133a633fe241ee598e8911e8f
GopiPallapu <140641025+GopiPallapu@users.noreply.github.com>
2023-10-02 17:14:36 +0530

    Time: 4 ms (93.3%), Space: 21.1 MB (43.33%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,18 +11,21 @@
  */
 class Solution {
 public:
+    bool flag = true ;
    int  helper(TreeNode*  root)
     {
         if(root == NULL)
             return 0;
         int leftH = helper(root->left);
-        if(leftH == -1)return -1;
         int rightH =helper(root->right);
-        if(rightH == -1)return -1;
-        if(abs(leftH - rightH) > 1)return -1;
+        if(abs(leftH - rightH) > 1)
+        {
+            flag = false;
+        }
         return max(leftH,rightH)+1 ;
     }
 	bool isBalanced(TreeNode*  root) {
-	    return helper(root)  != -1;
+	   helper(root);
+       return flag ;
 	}
 };
