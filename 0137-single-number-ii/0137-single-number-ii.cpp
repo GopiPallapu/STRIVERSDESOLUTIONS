bc7f63b6b2dc8411eb5b7015ba4204b388bd18cd
GopiPallapu <140641025+GopiPallapu@users.noreply.github.com>
2023-10-12 16:50:21 +0530

    Time: 8 ms (43.53%), Space: 9.8 MB (56.3%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    bool checkBit(int num , int i)
+    bool checkBit(int n , int i)
     {
-        if((num & (1 << i)) == 0)
+        if( (n & (1<<i)) == 0)
             return false;
         else
             return true;
     }
     int singleNumber(vector<int>& nums) {
-        int answer = 0;
-         //  get count of each bits in the num and take mood 
-         // if mod gives 1 then return that num
-
-         for(int i = 0; i < 32 ; i++)
-         {
-             int count =0;
-             for(int j = 0 ; j< nums.size() ; j++)
-             {
-                 if(checkBit(nums[j] , i) == true)
-                 {
-                     count ++ ;
-                 }
-             }
-             if(count % 3 == 1)
-             {
-                 answer = answer + ( 1<<i) ;
-             }
-         }
-         return answer ;
+        int answer =0 ;
+        for(int i =0 ;i< 32 ; i++)
+        {
+            int count =0;
+            for(int j =0 ;j< nums.size() ; j++)
+            {
+                if(checkBit(nums[j] , i) == true)
+                {
+                    count +=1 ;
+                }
+            }
+            if(count % 3 == 1)
+            {
+                answer = answer + (1<<i) ;
+            }
+        }
+        return answer ;
     }
 };
