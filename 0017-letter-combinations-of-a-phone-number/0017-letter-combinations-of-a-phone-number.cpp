7cda29600fb95a6364b05313227846ef8e9989de
GopiPallapu <140641025+GopiPallapu@users.noreply.github.com>
2023-10-16 18:21:33 +0530

    Time: 0 ms (100%), Space: 7 MB (27.52%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
-    void helper(int i , int n , string& temp ,vector<string>& answer , string& digits , unordered_map<char,string>& mp)
+    void getTotalCombinations(int i , string& str , vector<string>& answer , map<char,string>& mp, string digits)
     {
-        // base case
-        if(i == n)
+        if( i == digits.size())
         {
-            answer.push_back(temp);
+            answer.push_back(str) ;
             return ;
         }
-        string str = mp[digits[i]];
-        for(int k =0 ; k< str.size() ; k++)
+        
+        string temp = mp[digits[i]] ;
+        for(int j =0 ; j < temp.size() ; j++)
         {
-            temp.push_back(str[k]) ;
-            helper(i+1 , n , temp ,answer , digits , mp);
-            temp.pop_back() ;
+            str.push_back(temp[j]);
+            getTotalCombinations(i+1 , str , answer , mp , digits) ;
+            str.pop_back() ;
         }
         return ;
     }
     vector<string> letterCombinations(string digits) {
-        unordered_map<char ,string> mp;
-        mp['2'] = "abc";
-        mp['3'] = "def";
-        mp['4'] = "ghi";
-        mp['5'] = "jkl";
-        mp['6'] = "mno";
-        mp['7'] = "pqrs";
-        mp['8'] = "tuv";
-        mp['9'] = "wxyz";
+        map<char , string> mp ;
+        mp['2'] = "abc" ;
+        mp['3'] = "def" ;
+        mp['4'] = "ghi" ;
+        mp['5'] = "jkl" ;
+        mp['6'] = "mno" ;
+        mp['7'] = "pqrs" ;
+        mp['8'] = "tuv" ;
+        mp['9'] = "wxyz" ;
         
+        string str ;
         vector<string> answer ;
-        string temp ;
-        int n = digits.size() ;
         if(digits == "")
-            return {};
-        helper(0 , n , temp , answer , digits , mp );
+            return answer ;
+        getTotalCombinations(0,str , answer , mp, digits);
         return answer ;
     }
 };
