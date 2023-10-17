9346f6c524fc2e7bfac5cb8350195f3d46b52e87
GopiPallapu <140641025+GopiPallapu@users.noreply.github.com>
2023-10-17 14:18:04 +0530

    Time: 0 ms (100%), Space: 6.6 MB (56.24%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.size()-1 ;
+        while(n >= 0&& s[n]==' ')
+        {
+             n--;
+        }
+        int answer =0 ;
+        while(n >= 0 && s[n] !=' ')
+        {
+            answer +=1 ;
+            n--;
+        }
+        return answer ;
+    }
+};
