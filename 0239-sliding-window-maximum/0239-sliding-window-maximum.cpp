9f2c7134897e9d4467e5b846ce6c6c56821816a0
GopiPallapu <140641025+GopiPallapu@users.noreply.github.com>
2023-10-31 12:59:22 +0530

    Time: 190 ms (90.86%), Space: 136.9 MB (37.21%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n = nums.size();
-        deque<int> dq(k ,0) ;
+        deque<int> dq(k,0) ;
         vector<int> answer ;
-        // push first k elements to dq and compare elements which is max and remove small elements
-        for(int i =0 ;i < k; i++)
+        for(int i =0 ;i< k; i++)
         {
             while(!dq.empty() && nums[dq.back()] <= nums[i])
             {
-                dq.pop_back();
+                dq.pop_back() ;
             }
-            dq.push_back(i);
+            dq.push_back(i) ;
         }
-        // after first k elemets insertion queue contains max element index at front
-        // add that element to answer
-        answer.push_back(nums[dq.front()]);
-        // iterate through next n-k elements and find the max in each window
-        for(int i = k ; i < n ; i++)
+        answer.push_back(nums[dq.front()]) ;
+        
+        for(int i = k ; i< nums.size() ; i++)
         {
-            // remove indexs which are out of window range
-            while(!dq.empty() && dq.front() <= i-k)
+            if(!dq.empty() && dq.front() <= i-k)
+                dq.pop_front() ;
+            while(!dq.empty() && nums[dq.back()] < nums[i])
             {
-                dq.pop_front();
+                dq.pop_back() ;
             }
-            // removes indexs whose val is smaller than current nums[i]
-            while(!dq.empty() && nums[i] > nums[dq.back()])
-            {
-                dq.pop_back();
-            }
-            dq.push_back(i);
-
-            // for this window the max element index is at front
-            answer.push_back(nums[dq.front()]);
+            dq.push_back(i) ;
+        
+             answer.push_back(nums[dq.front()]) ;
         }
-        return answer;
+        return answer ;
     }
 };
